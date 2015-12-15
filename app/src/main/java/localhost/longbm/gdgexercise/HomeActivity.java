bc4274a681f664a245084163b1df87d701821278
longbm <minhlong139@gmail.com>
2015-12-16 01:25:42 +0700

    make item view

--- app/src/main/java/localhost/longbm/gdgexercise/HomeActivity.java
+++ app/src/main/java/localhost/longbm/gdgexercise/HomeActivity.java
@@ -1,7 +1,22 @@
 package localhost.longbm.gdgexercise;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.InputStream;
+import java.net.URL;
+
+import localhost.longbm.gdgexercise.model.Article;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,5 +29,36 @@ public class HomeActivity extends AppCompatActivity {
 
     protected void createList() {
 
+    }
+
+    private static class ArticleAdapter extends ArrayAdapter<Article> {
+
+        public ArticleAdapter(Context context, int resource) {
+            super(context, resource);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            if (convertView == null) {
+                LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+                convertView = layoutInflater.inflate(R.layout.article, parent, false);
+            }
+            Article article = getItem(position);
+
+            TextView tvTitle = (TextView) convertView.findViewById(R.id.tv_title);
+            tvTitle.setText(article.getTitle());
+
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.iv_image);
+            try {
+                URL url = new URL(article.getImage());
+                Bitmap bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                imageView.setImageBitmap(bmp);
+            } catch (Exception ex) {
+                int imageResource = parent.getResources().getIdentifier("@drawable/artichle", null, null);
+                imageView.setImageResource(imageResource);
+            }
+
+            return convertView;
+        }
     }
 }
---
+++ app/src/main/java/localhost/longbm/gdgexercise/model/Article.java
@@ -0,0 +1,70 @@
+package localhost.longbm.gdgexercise.model;
+
+/**
+ * Created by longbm on 12/16/15.
+ */
+public class Article {
+    private String id;
+    private String title;
+    private String image;
+    private String description;
+    private String author;
+    private String created;
+
+    public Article(String id, String title, String image, String description, String author, String created) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.description = description;
+        this.author = author;
+        this.created = created;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
